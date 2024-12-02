c26e1cab6a0e840287abb40f7417d0de69ea9894
Dmitry <rasma1@mail.ru>
2024-12-02 22:25:16 +0300

    Homework

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name",
+            defaultValue = "программист Java Spring")
+                            String name) {
+        return String.format("Привет, %s!", name);
+    }
+}
